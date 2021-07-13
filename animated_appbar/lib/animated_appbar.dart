7fca44aa620c5bd50c791be9e5a29082d84956f8
namkyu1999 <53862866+namkyu1999@users.noreply.github.com>
2021-07-13 06:00:22 +0000

    Add StackScaffold

--- animated_appbar/lib/animated_appbar.dart
+++ animated_appbar/lib/animated_appbar.dart
@@ -1,14 +1,49 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
 
-import 'dart:async';
-
-import 'package:flutter/services.dart';
-
-class AnimatedAppbar {
-  static const MethodChannel _channel =
-      const MethodChannel('animated_appbar');
-
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
-  }
+class StackScaffold extends Scaffold {
+  StackScaffold(
+    {Key? key,
+    PreferredSizeWidget? appBar, 
+    Widget? body, Widget? 
+    floatingActionButton, 
+    FloatingActionButtonLocation? floatingActionButtonLocation, 
+    FloatingActionButtonAnimator? floatingActionButtonAnimator, 
+    List<Widget>? persistentFooterButtons, 
+    Widget? drawer, 
+    DrawerCallback? onDrawerChanged, 
+    Widget? endDrawer, 
+    DrawerCallback? onEndDrawerChanged, 
+    Widget? bottomNavigationBar, 
+    Widget? bottomSheet, 
+    Color? backgroundColor, 
+    bool? resizeToAvoidBottomInset, 
+    bool primary = true, 
+    DragStartBehavior drawerDragStartBehavior = DragStartBehavior.start,
+    Color? drawerScrimColor, 
+    double? drawerEdgeDragWidth,
+    String? restorationId}):super(
+      extendBodyBehindAppBar: true,
+      key: key,
+      appBar: appBar, 
+      body: body,
+      floatingActionButton: floatingActionButton, 
+      floatingActionButtonLocation: floatingActionButtonLocation, 
+      floatingActionButtonAnimator: floatingActionButtonAnimator, 
+      persistentFooterButtons: persistentFooterButtons, 
+      drawer: drawer, 
+      onDrawerChanged: onDrawerChanged, 
+      endDrawer: endDrawer, 
+      onEndDrawerChanged: onEndDrawerChanged, 
+      bottomNavigationBar: bottomNavigationBar, 
+      bottomSheet: bottomSheet, 
+      backgroundColor: backgroundColor, 
+      resizeToAvoidBottomInset: resizeToAvoidBottomInset, 
+      primary: primary, 
+      drawerDragStartBehavior: drawerDragStartBehavior,
+      drawerScrimColor: drawerScrimColor, 
+      drawerEdgeDragWidth: drawerEdgeDragWidth,
+      restorationId: restorationId
+  );
 }
+
